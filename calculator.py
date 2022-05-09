1e319ba1aee03262b6df5d4ddd07fcba82d8a266
coolgaming33350 <carter.spenner@gmail.com>
2022-05-09 09:45:23 -0600

    added calculator.py
    Signed-off-by: coolgaming33350 <carter.spenner@gmail.com>

---
+++ calculator.py
@@ -0,0 +1,18 @@
+import random
+import time
+
+addlist = '+', 'addition', 'add', 'plus'
+sublist = '-', 'subtract', 'minus', 'sub'
+mullist = 'x', '*', 'multiply', 'mul'
+divlist = '/', 'divide', 'div'
+mathtype = input('What type of math would you like to do?: ')
+if mathtype in addlist:
+
+elif
+
+elif
+
+elif
+
+elif
+
